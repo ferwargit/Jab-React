d22bc91d85d0674507187976c3b5abf279e1c366
Fernando Warno <ferwar@hotmail.com>
2022-12-22 04:45:22 -0300

    video_01

---
+++ video_01/mi-proyecto/src/App.js
@@ -0,0 +1,29 @@
+// 1ro aplica el estilo de App.css
+import './App.css';
+// 2do aplica el estilo de style.css
+import './css/style.css';
+import imagen from './img/descarga.png';
+
+function App() {
+
+  let nombre = "Fernando";
+  let apellido = ['Herrera', 'García', 'González'];
+  let direccion = {calle: 'Calle 1', numero: 123	};
+  let caja = <div>Aquí hay un div</div>
+
+  return (
+    <div className="fondo">
+      <h1>Curso de React</h1>
+      <div>{nombre}</div>
+      <h2>{apellido[1]}</h2>
+      <h3>{direccion.calle} {direccion.numero}</h3>
+      <div>{caja}</div>
+      <img src={imagen} className="img" />
+      <input type="text" />
+      <input value={nombre} />
+      <br />
+    </div>
+  );
+}
+
+export default App;
