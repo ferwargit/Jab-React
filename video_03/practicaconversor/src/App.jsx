39451ffc1774c99c2673698318eb2f7b324435c6
Fernando Warno <ferwar@hotmail.com>
2022-12-31 01:28:38 -0300

    video03 conversor completo

--- video_03/practicaconversor/src/App.jsx
+++ video_03/practicaconversor/src/App.jsx
@@ -1,5 +1,6 @@
 // https://www.html6.es/curso/conversor.txt
 
+import { useState } from 'react';
 import "./App.css";
 
 const App = () => {
@@ -22,10 +23,36 @@ const App = () => {
     }
   ]
 
+  const [cantidad, setCantidad] = useState(0);
+  const [resultado1, setResultado1] = useState(0);
+  const [resultado2, setResultado2] = useState(0);
+  const [resultado3, setResultado3] = useState(0);
+  const [resultado4, setResultado4] = useState(0);
+
+  const modificar = (e) => {
+    // Hasta que no llega al fin no se modifica
+    setCantidad(e.target.value);
+    // setResultado1(cambios[1].cambio * cantidad);
+    // La cantidad tenemos que obtenerla directamente
+    setResultado1(cambios[1].cambio * e.target.value);
+    setResultado2(cambios[2].cambio * e.target.value);
+    setResultado3(cambios[3].cambio * e.target.value);
+    setResultado4(cambios[4].cambio * e.target.value);
+  }
+
   return (
     <>
-        <label htmlFor="u0">Etiqueta</label>
-        <input id="u0" type="number"/> 
+        <label htmlFor="u0">{cambios[0].moneda}</label>
+        <input id="u0" type="number" value={cantidad} onChange={modificar} />
+
+        <label htmlFor="u1">{cambios[1].moneda}</label>
+        <input id="u1" type="number" value={resultado1} readOnly /> 
+        <label htmlFor="u2">{cambios[2].moneda}</label>
+        <input id="u2" type="number" value={resultado2} readOnly /> 
+        <label htmlFor="u3">{cambios[3].moneda}</label>
+        <input id="u3" type="number" value={resultado3} readOnly /> 
+        <label htmlFor="u4">{cambios[4].moneda}</label>
+        <input id="u4" type="number" value={resultado4} readOnly /> 
     </>
   )
 }
